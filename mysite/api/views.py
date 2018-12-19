5e0b129721c497c7f70e7f50bf7bebe949a738b6
rodrigo.deoliveiracoelho <rodrigo.deoliveiracoelho@thomsonreuters.com>
2018-12-19 11:37:01 -0500

    added POST ability

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
-from flask import jsonify
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 tasks = [
     {
@@ -22,13 +23,33 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+# ------------------------------------------------------------- #
+# ------------------------ READ/UPDATE ------------------------ #
+# ------------------------------------------------------------- #
+@csrf_exempt
 def get_tasks(request):
-    """curl -i http://localhost:5000/api/tasks"""
-    return JsonResponse({'tasks': tasks})
+
+    if request.method == "GET":
+        """curl -i http://localhost:5000/api/tasks"""
+        return JsonResponse({'tasks': tasks})
+
+    elif request.method == "POST":
+        """curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:8000/api/tasks/"""  # noqa
+        request_json_body = json.loads(request.body)
+        if not request_json_body or 'title' not in request_json_body:
+            return JsonResponse({'error': 'null'})
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request_json_body['title'],
+            'description': request_json_body.get("description", ""),
+            'done': False
+                }
+        tasks.append(task)
+        return JsonResponse({'task': task})
 
 
 def get_task(request, task_id):
-    """curl -i http://localhost:5000/api/tasks/3"""
+    """curl -i http://localhost:8000/api/tasks/3"""
     task = [task for task in tasks if task_id == task['id']]
     if len(task) == 0:
         return JsonResponse({'error': 'Not Found'})
