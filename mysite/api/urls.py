4d49eb1740a592ac96e932f741a4ff92aa0dadf9
rodrigo.deoliveiracoelho <rodrigo.deoliveiracoelho@thomsonreuters.com>
2018-12-19 10:39:56 -0500

    added R in CRUD from flask_api

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tasks', views.get_tasks, name='get_tasks'),
+    path('tasks/<int:task_id>', views.get_task, name='get_task'),
 ]
 
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,7 +1,36 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from flask import jsonify
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def get_tasks(request):
+    """curl -i http://localhost:5000/api/tasks"""
+    return JsonResponse({'tasks': tasks})
+
+
+def get_task(request, task_id):
+    """curl -i http://localhost:5000/api/tasks/3"""
+    task = [task for task in tasks if task_id == task['id']]
+    if len(task) == 0:
+        return JsonResponse({'error': 'Not Found'})
+    return JsonResponse({'task': task[0]})
 
